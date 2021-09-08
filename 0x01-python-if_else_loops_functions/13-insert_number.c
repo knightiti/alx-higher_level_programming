aa3f8a16d2fb554833f8fce0c65bf4ec22e347aa
Knightitie <ktess2008@gmail.com>
2021-09-08 17:45:26 +0000

    <fixed errors>

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * insert_node - a func that inserts a number in a list
@@ -9,29 +9,28 @@
  */
 
 listint_t *insert_node(listint_t **head, int number)
-
 {
-  listint_t *node = *head;
-  listint_t *new;
+listint_t *node = *head;
+listint_t *new;
 
- new = malloc(sizeof(listint_t));
+new = malloc(sizeof(listint_t));
 
-  if (new == NULL)
-    return (NULL);
+if (new == NULL)
+return (NULL);
 
-  new->n = number;
+new->n = number;
 
-  if (node == NULL || node->n >= number)
-    {
-      new->next = node;
-      *head = new;
-      return (new);
-    }
-  while (node && node->next && node->next->n < number)
+if (node == NULL || node->n >= number)
+{
+new->next = node;
+*head = new;
+return (new);
+}
+while (node && node->next && node->next->n < number)
 
-    node = node->next;
-  new->next = node->next;
-  node->next = new;
+node = node->next;
+new->next = node->next;
+node->next = new;
 
-  return (new);
+ return (new);
 }
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 for i in range(100):
     if not (i == 99):
         print("{:02}".format(i), end=", ")

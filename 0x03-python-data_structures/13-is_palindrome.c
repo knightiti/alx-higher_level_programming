0bf773352412c13af406ad9d537f5e49061ba3d2
knightiti <ktess2008@gmail.com>
2021-09-14 14:52:50 -0700

    <Linked list palindrome>

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a list is parlindrome
+ * @head: head of linked list
+ *
+ * Return: 0 if false and 1 if true
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *node;
+	int values[9999], i = 0, c = 0;
+
+	if ((!*head) || (!head))
+	{
+		return (1);
+	}
+	node = *head;
+	if (!node->next)
+	{
+		return (1);
+	}
+	while (node)
+	{
+		values[i] = node->n;
+		node = node->next;
+		i++;
+	}
+	i--;
+	while (i >= 0 && c <= i)
+	{
+		if (values[i] != values[c])
+		{
+			return (0);
+		}
+		i--;
+		c++;
+	}
+	return (1);
+}

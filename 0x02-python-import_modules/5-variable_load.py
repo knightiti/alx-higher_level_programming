753084a69e1a90e1be72de81dcbaa67cf7e1243a
knightiti <ktess2008@gmail.com>
2021-09-15 16:01:10 -0700

    <fixed code>

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-def main():
 
-
-    print("{}".format(a))
 if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-
-    main()
+    print(a)

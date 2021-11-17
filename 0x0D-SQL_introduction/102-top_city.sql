c4d3ecea21745a237c2c870a1a12b9e0284d8381
knightiti <ktess2008@gmail.com>
2021-11-16 19:12:06 -0800

    <Temperatures #1>

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,10 @@
+-- Import in hbtn_0c_0 database this table dump: Temperatures
+-- A script to display the top 3 of cities temperature during
+-- July and August in descending order.
+
+SELECT city, AVG(value) as avg_temp
+FROM Temperatures
+WHERE month = '7' OR month = '8'
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;

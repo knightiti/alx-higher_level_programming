4610c6e276588e07eea6d4c7ed7984bef5bdd2c8
knightiti <ktess2008@gmail.com>
2021-11-18 10:22:07 -0800

    <Fixed error>

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,7 @@
 -- A script lists all records of secong_table in the database hbtn_0c_0
---Results should display both the score and the name (in this order)
---Records should be ordered by score (top first)
+-- Results should display the score, then the name.
+-- The score should be in descending order.
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score,
+name FROM second_table
+ORDER BY score DESC;

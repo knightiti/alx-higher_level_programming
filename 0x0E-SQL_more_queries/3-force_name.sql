eca4c8bfce0a9154748f2599ff1a8197481d4a32
knightiti <ktess2008@gmail.com>
2021-11-18 11:23:54 -0800

    <fixed code>

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- A script that creates the table force_name on your MySQL server.
 -- use the NOT NULL constraint for the name column.
 
-CREATE TABLE IF NOT EXISTS force_name (Id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);

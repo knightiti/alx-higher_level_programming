55a6ce4f77fa01af656f26a3cfdd3b244af5e6db
knightiti <ktess2008@gmail.com>
2021-11-17 17:33:51 -0800

    <SQL databases tables and constraints>

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- A script that lists all privileges of the user_0d_1 and user_0d_2 in localhost.
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
+
+
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- A script that creates the table force_name on your MySQL server.
+-- use the NOT NULL constraint for the name column.
+
+CREATE TABLE IF NOT EXISTS force_name (ID INT, name VARCHAR(256) NOT NULL);
